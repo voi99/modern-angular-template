c5a97ebaf932cf36696c8c73462681c924103d76
voi99 <ivanivopop@gmail.com>
2024-07-30 21:15:06 +0200

    upd: migrate to zoneless

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,9 @@
-import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from "@angular/core";
 import { provideRouter } from "@angular/router";
 
 import { routes } from "./app.routes";
 import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()],
+  providers: [provideExperimentalZonelessChangeDetection(), provideRouter(routes), provideAnimationsAsync()],
 };
