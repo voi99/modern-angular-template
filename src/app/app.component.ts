e674cde288c4cd528a5d84e0b3b90588d29d7ef1
voi99 <ivanivopop@gmail.com>
2024-07-30 21:19:06 +0200

    upd: cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
-import { Component } from "@angular/core";
+import { ChangeDetectionStrategy, Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: "app-root",
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: "./app.component.html",
-  styleUrl: "./app.component.scss",
+  template: ``,
+  styles: ``,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
-  title = "modern-angular-template";
-}
+export class AppComponent {}
