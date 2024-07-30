3dccd139bc9f1e77ba7f55f4918ffccc9cd0ed80
voi99 <ivanivopop@gmail.com>
2024-07-30 21:31:43 +0200

    add: buttons color variants

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,12 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
+import { MatButton } from "@angular/material/button";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet],
-  template: `
-    <div class="bg-primary-container text-on-primary-container">
-    Test
-    </div>
-  `,
+  imports: [RouterOutlet, MatButton],
+  template: ` <button mat-flat-button class="mat-tonal">Test</button> `,
   styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
