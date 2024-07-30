0ad8c0d20eb7e5a949588e04cdbf2c49a81c0eb1
voi99 <ivanivopop@gmail.com>
2024-07-30 22:28:19 +0200

    add: chips color variants

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,23 @@ import { MatButton, MatIconButton, MatMiniFabButton } from "@angular/material/bu
 import { MatIcon } from "@angular/material/icon";
 import { MatFormField, MatLabel } from "@angular/material/form-field";
 import { MatInput } from "@angular/material/input";
+import { MatChipListbox, MatChipOption } from "@angular/material/chips";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet, MatButton, MatIconButton, MatIcon, MatFormField, MatInput, MatLabel, MatMiniFabButton],
+  imports: [
+    RouterOutlet,
+    MatButton,
+    MatIconButton,
+    MatIcon,
+    MatFormField,
+    MatInput,
+    MatLabel,
+    MatMiniFabButton,
+    MatChipListbox,
+    MatChipOption,
+  ],
   template: `
     <div class="bg-surface-container p-2 flex items-center gap-2 justify-center flex-wrap">
       <button mat-flat-button class="mat-xs">Test</button>
@@ -44,6 +56,11 @@ import { MatInput } from "@angular/material/input";
         <mat-label>Email</mat-label>
         <input matInput />
       </mat-form-field>
+      <mat-chip-listbox class="mat-mdc-chip-set-stacked" aria-label="Chips">
+        <mat-chip-option selected class="mat-primary">Primary</mat-chip-option>
+        <mat-chip-option selected>Secondary</mat-chip-option>
+        <mat-chip-option selected class="mat-tertiary">Tertiary</mat-chip-option>
+      </mat-chip-listbox>
     </div>
   `,
   styles: ``,
