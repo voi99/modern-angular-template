06f3b74f6af424081b188839df5eddd5a952e287
voi99 <ivanivopop@gmail.com>
2024-07-31 19:26:11 +0200

    upd: buttons example

--- src/app/components/buttons/buttons.component.ts
+++ src/app/components/buttons/buttons.component.ts
@@ -8,11 +8,14 @@ import { CardComponent } from "../card/card.component";
   imports: [MatButton, CardComponent],
   template: `
     <app-card cardTitle="Buttons">
-      <div class="flex gap-2 flex-wrap items-center">
-        <button mat-flat-button class="mat-xs">Test</button>
-        <button mat-flat-button class="mat-small">Test</button>
-        <button mat-flat-button class="mat-medium">Test</button>
-        <button mat-flat-button>Test</button>
+      <div class="flex flex-col gap-4">
+        <div class="flex gap-2 flex-wrap items-center">
+          <button mat-flat-button class="mat-xs mat-tertiary">Test</button>
+          <button mat-flat-button class="mat-small mat-error">Test</button>
+          <button mat-flat-button class="mat-medium">Test</button>
+          <button mat-flat-button class="mat-secondary">Test</button>
+          <button mat-flat-button class="mat-tonal">Test</button>
+        </div>
       </div>
     </app-card>
   `,
