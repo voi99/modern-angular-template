d7a98d3e19c2a53de3837ad57d4ce87da8eef9a7
voi99 <ivanivopop@gmail.com>
2025-01-02 22:44:15 +0100

    chore: minor improvements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,17 +12,17 @@ import { TailwindMaterialExampleComponent } from "./examples/tailwind-material/t
         @defer {
           <tailwind-material-example />
         } @placeholder {
-          <div class="h-96 animate-pulse rounded-xl bg-surface-dim"></div>
+          <div class="h-[700px] animate-pulse rounded-xl bg-surface-dim md:h-96"></div>
         }
         @defer {
           <form-validation-example />
         } @placeholder {
-          <div class="h-96 animate-pulse rounded-xl bg-surface-dim"></div>
+          <div class="h-[600px] animate-pulse rounded-xl bg-surface-dim md:h-80"></div>
         }
         @defer {
           <buttons-example />
         } @placeholder {
-          <div class="h-28 animate-pulse rounded-xl bg-surface-dim"></div>
+          <div class="h-52 animate-pulse rounded-xl bg-surface-dim md:h-28"></div>
         }
       </div>
     </div>
