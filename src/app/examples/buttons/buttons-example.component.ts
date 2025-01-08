fd1320f260f1f818ccbb2461b2ccaa9a82f62785
voi99 <ivanivopop@gmail.com>
2025-01-08 17:28:25 +0100

    chore: refactor to mat overrides mixins

--- src/app/examples/buttons/buttons-example.component.ts
+++ src/app/examples/buttons/buttons-example.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
-import { MatButton } from "@angular/material/button";
+import { MatButton, MatIconButton } from "@angular/material/button";
 import { CardComponent } from "../card/card.component";
+import { MatIcon } from "@angular/material/icon";
 
 @Component({
   selector: "buttons-example",
-  imports: [MatButton, CardComponent],
+  imports: [MatButton, CardComponent, MatIconButton, MatIcon],
   template: `
     <card cardTitle="Buttons">
       <div class="flex flex-wrap items-center gap-2">
@@ -13,6 +14,23 @@ import { CardComponent } from "../card/card.component";
         <button mat-flat-button class="mat-md">default</button>
         <button mat-flat-button class="mat-secondary">mat-secondary</button>
         <button mat-flat-button class="mat-tonal">mat-tonal</button>
+      </div>
+      <div class="mt-2 flex flex-wrap items-center gap-2">
+        <button mat-icon-button>
+          <mat-icon>settings</mat-icon>
+        </button>
+        <button mat-icon-button class="mat-primary">
+          <mat-icon>settings</mat-icon>
+        </button>
+        <button mat-icon-button class="mat-secondary">
+          <mat-icon>settings</mat-icon>
+        </button>
+        <button mat-icon-button class="mat-tertiary">
+          <mat-icon>settings</mat-icon>
+        </button>
+        <button mat-icon-button class="mat-error">
+          <mat-icon>settings</mat-icon>
+        </button>
       </div>
     </card>
   `,
