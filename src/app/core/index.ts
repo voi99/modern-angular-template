4a07805ad1b5f94edfde48b0cf504c85b69fe5b9
voi99 <ivanivopop@gmail.com>
2025-02-27 18:44:00 +0100

    chore: remove animations provider

--- src/app/core/index.ts
+++ src/app/core/index.ts
@@ -1,8 +1,7 @@
 import { Provider, EnvironmentProviders, provideExperimentalZonelessChangeDetection } from "@angular/core";
-import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
 import { provideRouter } from "@angular/router";
 import { routes } from "../app.routes";
 
 export function provideCore(): (Provider | EnvironmentProviders)[] {
-  return [provideExperimentalZonelessChangeDetection(), provideRouter(routes), provideAnimationsAsync()];
+  return [provideExperimentalZonelessChangeDetection(), provideRouter(routes)];
 }
