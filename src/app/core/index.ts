c0fce8c2d00445278de57bdffd80a3598f753a09
voi99 <ivanivopop@gmail.com>
2025-09-10 18:26:57 +0200

    chore: update angular

--- src/app/core/index.ts
+++ src/app/core/index.ts
@@ -1,7 +1,7 @@
-import { Provider, EnvironmentProviders, provideExperimentalZonelessChangeDetection } from "@angular/core";
+import { Provider, EnvironmentProviders, provideZonelessChangeDetection } from "@angular/core";
 import { provideRouter } from "@angular/router";
 import { routes } from "../app.routes";
 
 export function provideCore(): (Provider | EnvironmentProviders)[] {
-  return [provideExperimentalZonelessChangeDetection(), provideRouter(routes)];
+  return [provideZonelessChangeDetection(), provideRouter(routes)];
 }
