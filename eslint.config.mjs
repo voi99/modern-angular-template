942dc7626113a4b81368d1a82ae859d82cee7f71
voi99 <ivanivopop@gmail.com>
2026-01-14 20:33:49 +0100

    chore: migrate to defineConfig (eslint)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,53 +1,52 @@
 // @ts-check
+import { defineConfig } from "eslint/config";
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import angular from "angular-eslint";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
-export default [
-  ...tseslint.config(
-    {
-      files: ["**/*.ts"],
-      extends: [
-        eslint.configs.recommended,
-        ...tseslint.configs.recommended,
-        ...tseslint.configs.stylistic,
-        ...angular.configs.tsRecommended,
+export default defineConfig([
+  {
+    files: ["**/*.ts"],
+    extends: [
+      eslint.configs.recommended,
+      tseslint.configs.recommended,
+      tseslint.configs.stylistic,
+      angular.configs.tsRecommended,
+    ],
+    processor: angular.processInlineTemplates,
+    rules: {
+      "@angular-eslint/directive-selector": [
+        "error",
+        {
+          type: "attribute",
+          style: "camelCase",
+          prefix: "",
+        },
       ],
-      processor: angular.processInlineTemplates,
-      rules: {
-        "@angular-eslint/directive-selector": [
-          "error",
-          {
-            type: "attribute",
-            style: "camelCase",
-            prefix: "",
-          },
-        ],
-        "@angular-eslint/component-selector": [
-          "error",
-          {
-            type: "element",
-            style: "kebab-case",
-            prefix: "",
-          },
-        ],
-      },
+      "@angular-eslint/component-selector": [
+        "error",
+        {
+          type: "element",
+          style: "kebab-case",
+          prefix: "",
+        },
+      ],
     },
-    {
-      files: ["**/*.html"],
-      extends: [...angular.configs.templateRecommended, ...angular.configs.templateAccessibility],
-      rules: {
-        "prettier/prettier": [
-          "error",
-          {
-            parser: "angular",
-          },
-        ],
-        "@angular-eslint/template/prefer-self-closing-tags": "error",
-        "@angular-eslint/template/prefer-control-flow": "error",
-      },
-    }
-  ),
+  },
+  {
+    files: ["**/*.html"],
+    extends: [angular.configs.templateRecommended, angular.configs.templateAccessibility],
+    rules: {
+      "prettier/prettier": [
+        "error",
+        {
+          parser: "angular",
+        },
+      ],
+      "@angular-eslint/template/prefer-self-closing-tags": "error",
+      "@angular-eslint/template/prefer-control-flow": "error",
+    },
+  },
   eslintPluginPrettierRecommended,
-];
+]);
